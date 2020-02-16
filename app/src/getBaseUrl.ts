fec5b75e2c4573328473df8986104b9fa90adba1
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-02-16 16:03:16 +0100

    code

--- app/src/getBaseUrl.ts
+++ app/src/getBaseUrl.ts
@@ -1,5 +1,13 @@
-export function getBaseUrl() {
-  
+export function getBaseUrl(url?: string) {
+  if (url === undefined) return window.location.origin
+
+  const urlAsArray = url.split('/');
+  const doubleSlashIndex = url.indexOf("://")
+  if (doubleSlashIndex !== -1 && doubleSlashIndex === url.indexOf("/")-1) urlAsArray.length = 3
+  else urlAsArray.length = 1
+  return urlAsArray.join("/")
 }
 
 export default getBaseUrl
+
+window.location
