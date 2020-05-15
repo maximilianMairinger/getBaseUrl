cf19663d77093e96666d02ffae25974ccce2115b
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-05-15 14:40:04 +0200

    rm debugger

--- app/src/getBaseUrl.ts
+++ app/src/getBaseUrl.ts
@@ -2,7 +2,7 @@ import { isBrowser } from "browser-or-node"
 
 export function getBaseUrl(url?: string) {
   if (url === undefined) return isBrowser ? window.location.origin : "/"
-  debugger
+  
   const urlAsArray = url.split('/');
   const doubleSlashIndex = url.indexOf("://")
   if (doubleSlashIndex !== -1 && doubleSlashIndex === url.indexOf("/")-1) urlAsArray.length = 3
